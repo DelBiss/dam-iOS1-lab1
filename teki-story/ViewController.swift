b636d6afc7aaa74517f8a560784a83a31bf6c4bf
Philippe Allard-Rousse <philrousse@gmail.com>
2022-02-16 11:31:16 -0500

    Initial Commit

---
+++ teki-story/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  teki-story
+//
+//  Created by Philippe Allard-Rousse (Étudiant) on 2022-02-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
