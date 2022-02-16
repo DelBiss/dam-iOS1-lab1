4ef47cecdea8532c9a25dd1d5d898018b59bba52
Philippe Allard-Rousse <philrousse@gmail.com>
2022-02-16 13:08:35 -0500

    Finished Tutorial

--- teki-story/ViewController.swift
+++ teki-story/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
+    let activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
     @IBOutlet weak var quoteLabel: UILabel!
-    
 
+    @IBAction func changeQuote() {
+        let celebrity = celebrities[Int(arc4random_uniform(UInt32(celebrities.count)))]
+        let activity = activities[Int(arc4random_uniform(UInt32(activities.count)))]
+        print("Youpi ! Notre action fonctionne parfaitement !")
+        
+        quoteLabel.text = "Tu es " + celebrity + " " + activity
+    }
+    
 
 }
 
